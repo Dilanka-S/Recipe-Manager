f2f42f267924e88ae767470cb95a621db457746c
Dilanka Seneviratne <dvseneviratne@gmail.com>
2023-02-23 18:21:00 +0530

    Connected to MongoDB through :5001
    Added MERN dependancies and made the connection to my MongoDB cluster through port 5001

---
+++ app.js
@@ -0,0 +1,26 @@
+console.log("MERN stack CRUD recipe management app for Code94 by Dilanka Vishad Seneviratne");
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//Middlewares
+app.use('/',(request, response, next)=>{
+    response.send("Welcome to the Recipe Manager")
+});
+
+//Connecting to the MongoDB server
+mongoose.connect(
+    "mongodb+srv://admin:2976AbkbhGV6NP9W@cluster0.of37cws.mongodb.net/recipeManager?retryWrites=true&w=majority"
+     )
+     .then(()=>console.log("Connected to the Recipe Manager database"))
+     .then(()=>{
+        app.listen(5001);
+     })
+     .catch((err)=> console.log(err));
+
+// Default created admin account details;
+// username : admin
+// admin password  : 2976AbkbhGV6NP9W
+//hello
